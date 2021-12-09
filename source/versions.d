fd6d05ace18d364c2116e13c9bb789ec551e8153
AntonC9018 <anton.curm@gmail.com>
2021-12-09 20:14:35 +0200

    Lib

---
+++ source/versions.d
@@ -0,0 +1,55 @@
+module acd.versions;
+
+/** 
+	Lets you get a given version as a boolean expression.
+
+	Examples:
+
+	```d
+	version = Hello;
+	static assert (Version.Hello && !Version.Foo);
+
+	// Setting version conditionally
+	static if (Version.Hello)
+		version = World;
+	static assert(Version.World);
+
+	// Setting version if a version is not defined
+	static if (!Version.A)
+		version = B;
+  	static if (Version.B)
+		version = C;
+    static assert(Version.C);
+
+	// Save as expressions
+	enum BC = Version.B && Version.C;
+	static assert(BC);
+	```
+
+	Note: the following compiles, because `version = Bar` is evaluated before
+	the template gets expanded. With a `version(Bar){}` it would just not compile,
+	because versions cannot be defined after they have been used.
+
+	```d
+	static assert(Version.Bar);
+	version = Bar;
+	```
+
+	Works when passing version flags when compiling as well.
+	
+	The other assumptions you have about the way this works will just hold true.
+	Surprisingly, it just works!
+*/
+struct Version
+{
+    @disable this();
+	static template opDispatch(string id)
+    {	
+        mixin(`
+            version (`, id, `) 
+              	enum opDispatch = true;
+            else 
+              	enum opDispatch = false;
+        `);
+    }
+}
